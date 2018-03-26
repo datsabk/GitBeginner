3f7d0cbcd10bc4c579444fb39231ed40d4b54285
Abhishek Kothari <abk.edu@gmail.com>
2018-03-27 01:16:00 +0530

    Added two lines

--- src/com/javacodegeeks/SayHello.java
+++ src/com/javacodegeeks/SayHello.java
@@ -3,7 +3,9 @@ package com.javacodegeeks;
 public class SayHello {
 	public static void main(String[] args) {
 		System.out.println("Hello there!");
+		System.out.println("Printing Even numbers");
 		EvenNumbers.printEvenNumber(11);
+		System.out.println("Printing Odd numbers");
 		OddNumbers.printOddNumber(12);
 	}
 }
